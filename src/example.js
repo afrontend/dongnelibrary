7cc94bdaa10c6b7fb50a2aed879948ed4e5f234f
afrontend <frontend.wordpress@gmail.com>
2017-06-22 21:56:26 +0900

    Update example.js

--- src/example.js
+++ src/example.js
@@ -1,8 +1,8 @@
 require("dongnelibrary").search({
     title: 'javascript',
     libraryName: '남양도서관'
-  }, function (books) {
-    books.forEach(function (book) {
-        console.log((book.exist?'책있음':'책없음') +' '+ book.title);
+  }, function (err, book) {
+    book.booklist.forEach(function (book) {
+        console.log((book.exist?' ✓  ':' ✖  ') +' '+ book.title);
     });
 });
