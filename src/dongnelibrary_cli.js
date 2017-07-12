590c9a67f151bb94f8d88f4bb6eca8104dbedb61
afrontend <frontend.wordpress@gmail.com>
2017-07-12 21:40:41 +0900

    v0.1.13

--- src/dongnelibrary_cli.js
+++ src/dongnelibrary_cli.js
@@ -7,7 +7,7 @@ var colors = require('colors');
 var async = require('async');
 
 program
-  .version('0.1.12')
+  .version('0.1.13')
   .option('-a, --all-library'                 , 'Show all library')
   .option('-j, --json-format'                 , 'JSON format')
   .option('-l, --library-name [value1,value2]', 'Add library name')
