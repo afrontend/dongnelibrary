2b133f586cc9269917133350bfd1195c2a387604
afrontend <frontend.wordpress@gmail.com>
2019-02-03 19:56:19 +0900

    update search style for gunpo

--- src/library/gunpo.js
+++ src/library/gunpo.js
@@ -6,13 +6,13 @@ var getLibraryNames = require('../dongnelibrary_util.js').getLibraryNames;
 var global = {};
 
 var libraryList = [
-  {code: '01', name: '군포시산본도서관'},
-  {code: '02', name: '군포시당동도서관'},
-  {code: '03', name: '군포시대야도서관'},
-  {code: '04', name: '군포시어린이도서관'},
-  {code: '05', name: '군포시이동도서관'},
-  {code: '06', name: '군포시중앙도서관'},
-  {code: '09', name: '군포시부곡도서관'},
+  {code: '1', name: '군포시산본도서관'},
+  {code: '2', name: '군포시당동도서관'},
+  {code: '3', name: '군포시대야도서관'},
+  {code: '4', name: '군포시어린이도서관'},
+  {code: '5', name: '군포시이동도서관'},
+  {code: '6', name: '군포시중앙도서관'},
+  {code: '9', name: '군포시부곡도서관'},
   {code: '10', name: '군포시당정문화도서관'},
   {code: '11', name: '군포시동화나무어린이도서관'},
   {code: '12', name: '군포시금정작은도서관'},
@@ -30,9 +30,6 @@ var libraryList = [
   {code: '24', name: '군포시여담작은도서관'},
 ];
 
-//document.querySelector('#searchResultInner > div.searchTitle5 > h3 > span').textContent
-//"(146건중 10건 출력)"
-
 function getLibraryCode(libraryName) {
   var found = _.find(libraryList, function (lib) {
     return lib.name === libraryName;
@@ -44,45 +41,20 @@ function getLibraryCode(libraryName) {
   return '';
 }
 
-function exist(str) {
-  return (str.indexOf('대출가능') !== -1);
+function getBookList(json) {
+  return _.map(json.data ? json.data.list : [], function (book) {
+    return {
+      title: book.titleStatement,
+      exist: _.some(book.branchVolumes, function (vol) { return vol.cState === '대출가능' }) === undefined ? false : true
+    };
+  });
 }
 
-function getTotalCount(str) {
-  var pattern = /^\((\d+)/g,
-      match;
-
-  match = pattern.exec(str);
-  if (match) {
-    return match[1];
-  } else {
-    return null;
-  }
-}
-
-function makeJsdomCallback(libraryName, getBook) {
-  return function(errors, window) {
-    var booklist = [];
-    var $ = window.$;
-    var totalBookCount = getTotalCount($('#searchResultInner > div.searchTitle5 > h3 > span').text().trim());
-    var $a = $('#frmResultList > ul > li');
-    _.each($a, function (value) {
-        var $value = $(value);
-        booklist.push({
-            libraryName: libraryName,
-            title: $value.find('.body > a.title').text().trim(),
-            exist: exist($value.find('.tag').text().trim())
-        });
-    });
-
-    if (getBook) {
-      getBook(null, {
-        totalBookCount: totalBookCount || booklist.length,
-        booklist: booklist
-      });
-    }
-    window.close();
-  }
+function addLibraryNameToBookList(libraryName, booklist) {
+  return _.map(booklist, function (book) {
+    book.libraryName = libraryName;
+    return book;
+  });
 }
 
 function search(opt, getBook) {
@@ -112,45 +84,42 @@ function search(opt, getBook) {
   }
 
   req.get({
-      url: 'http://www.gunpolib.go.kr/search/Search.Result.ax',
-      timeout: 20000,
-      headers: {
-        "User-Agent": 'User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36'
-      },
-      qs: {
-        sid: '1',
-        mf: 'true',
-        br: getLibraryCode(libraryName),
-        item: 'ALL',
-        value: title,
-        pageSize: 9999
-      }
-    }, function (err, res, body) {
-      if (global.debug === true) {
-        console.log(body);
-      }
-      if (err || (res && res.statusCode !== 200)) {
-        var msg = '';
-
-        if (err) {
-          msg = err;
-        }
-
-        if (res && res.statusCode) {
-          msg = msg + " " + res.statusCode;
-        }
-
-        if (getBook) {
-          getBook({msg: msg});
-        }
-      } else {
-         jsdom.env({
-             html: body,
-             src: [jquery],
-             done: makeJsdomCallback(libraryName, getBook)
-         });
-      }
+    url: 'https://www.gunpolib.go.kr/pyxis-api/1/collections/1/search',
+    timeout: 20000,
+    headers: {
+      "User-Agent": 'User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36'
+    },
+    qs: {
+      all: `k|a|${title}`,
+      branch: getLibraryCode(libraryName),
+      max: 200
     }
+  }, function (err, res, body) {
+    if (global.debug === true) {
+      console.log(body);
+    }
+    if (err || (res && res.statusCode !== 200)) {
+      var msg = '';
+
+      if (err) {
+        msg = err;
+      }
+
+      if (res && res.statusCode) {
+        msg = msg + " " + res.statusCode;
+      }
+
+      if (getBook) {
+        getBook({msg: msg});
+      }
+    } else {
+      var booklist = addLibraryNameToBookList(libraryName, getBookList(JSON.parse(body)));
+      getBook(null, {
+        totalBookCount: booklist.length,
+        booklist: booklist
+      });
+    }
+  }
   );
 }
 
