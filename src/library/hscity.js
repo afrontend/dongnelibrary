815d114c7813887de63431664794587050f5e1a0
afrontend <frontend.wordpress@gmail.com>
2019-02-03 21:11:30 +0900

    update search style for hscity

--- src/library/hscity.js
+++ src/library/hscity.js
@@ -31,21 +31,21 @@ function getLibraryCode(libraryName) {
   return '';
 }
 
-function exist(str) {
-  return !(str === '관외대출자료');
+function isRented(str) {
+  return !~str.indexOf('대출가능');
 }
 
 function makeJsdomCallback(libraryName, getBook) {
   return function(errors, window) {
     var booklist = [];
     var $ = window.$;
-    var $a = $('#wrap > div.data_type5.mb15 > form > table > tbody > tr');
+    var $a = $('.booklist > li');
     _.each($a, function (value) {
       var $value = $(value);
       booklist.push({
         libraryName: libraryName,
-        title: $value.find('td:nth-child(2) > a').text().trim(),
-        exist: exist($value.find('td:nth-child(8)').text().trim())
+        title: $value.find('ul > li > dl > dt > a').text().trim(),
+        exist: !isRented($value.find('ul > li > dl > dd > span').text().trim())
       });
     });
     if (getBook) {
@@ -85,24 +85,27 @@ function search(opt, getBook) {
   }
 
   req.post({
-    url: 'http://www.hscitylib.or.kr/UnitySearch/unity_library_result.do',
+    url: 'http://hscitylib.or.kr/kolaseek/booksearch/plusSearchResultList.do',
     timeout: 20000,
     headers: {
       "User-Agent": 'User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36'
     },
     form: {
-      beforequery: "IAL:" + title + "*",
-      sort: 'RK DESC',
-      mgc: getLibraryCode(libraryName),
-      jongbook: "_book",
-      historycount: "0",
-      startpage: "1",
-      limitpage: "100",
-      mode: '0'
+      searchType: 'SIMPLE',
+      searchCategory: 'ALL',
+      currentPageNo: 1,
+      viewStatus: 'IMAGE',
+      preSearchKey: 'ALL',
+      preSearchKeyword: 'javascript',
+      searchKey: 'ALL',
+      searchKeyword: 'javascript',
+      searchLibraryArr: getLibraryCode(libraryName),
+      searchSort: 'SIMILAR',
+      searchOrder: 'DESC',
     }
   }, function (err, res, body) {
-    if (global.debug === true) {
       console.log(body);
+    if (global.debug === true) {
     }
     if (err || (res && res.statusCode !== 200)) {
       var msg = '';
