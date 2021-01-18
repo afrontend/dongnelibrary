e933c43b50d11a5c4811d50580e39a6d743182a2
afrontend <autoscripts@gmail.com>
2021-01-18 21:50:09 +0900

    Remove ;

--- src/library/hscity.js
+++ src/library/hscity.js
@@ -1,9 +1,9 @@
-const jsdom = require('jsdom');
-const req = require('request');
-const _ = require('lodash');
-const jquery = require('../dongnelibrary_util').getJqueryString();
-const getLibraryNames = require('../dongnelibrary_util.js').getLibraryNames;
-const global = {};
+const jsdom = require('jsdom')
+const req = require('request')
+const _ = require('lodash')
+const jquery = require('../dongnelibrary_util').getJqueryString()
+const getLibraryNames = require('../dongnelibrary_util.js').getLibraryNames
+const global = {}
 
 const libraryList = [
   {code: 'MA', name: '남양도서관'},
@@ -21,77 +21,77 @@ const libraryList = [
   {code: 'MN', name: '비봉작은도서관'},
   {code: 'MO', name: '진안도서관'},
   {code: 'MP', name: '동탄중앙이음터도서관'}
-];
+]
 
 function getLibraryCode(libraryName) {
   const found = _.find(libraryList, function (lib) {
-    return lib.name === libraryName;
-  });
+    return lib.name === libraryName
+  })
 
   if (found) {
-    return found.code;
+    return found.code
   }
-  return '';
+  return ''
 }
 
 function isRented(str) {
-  return !~str.indexOf('대출가능');
+  return !~str.indexOf('대출가능')
 }
 
 function makeJsdomCallback(libraryName, getBook) {
   return function(errors, window) {
-    const booklist = [];
-    const $ = window.$;
-    const $a = $('.booklist > li');
+    const booklist = []
+    const $ = window.$
+    const $a = $('.booklist > li')
     _.each($a, function (value) {
-      const $value = $(value);
+      const $value = $(value)
       booklist.push({
         libraryName: libraryName,
         title: $value.find('ul > li > dl > dt > a').text().trim(),
         exist: !isRented($value.find('ul > li > dl > dd > span').text().trim())
-      });
-    });
+      })
+    })
     if (getBook) {
       getBook(null, {
         totalBookCount: booklist.length,
         booklist: booklist
-      });
+      })
     }
-    window.close();
-  };
+    window.close()
+  }
 }
 
 function search(opt, getBook) {
-  let title = '';
-  let libraryName = '';
+  let title = ''
+  let libraryName = ''
 
   if (opt.debug) {
-    global.debug = true;
+    global.debug = true
   }
 
   if (opt.title) {
-    title = opt.title;
+    title = opt.title
   } else {
     if (getBook) {
-      getBook({msg: 'Need a book name'});
+      getBook({msg: 'Need a book name'})
     }
-    return;
+    return
   }
 
   if (opt.libraryName) {
-    libraryName = opt.libraryName;
+    libraryName = opt.libraryName
   } else {
     if (getBook) {
-      getBook({msg: 'Need a library name'});
+      getBook({msg: 'Need a library name'})
     }
-    return;
+    return
   }
 
   req.post({
     url: 'http://hscitylib.or.kr/kolaseek/booksearch/plusSearchResultList.do',
     timeout: 20000,
     headers: {
-      "User-Agent": 'User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36'
+      "User-Agent": 'User-Agent:Mozilla/5.0 (X11 Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36'
     },
     form: {
       searchType: 'SIMPLE',
@@ -108,36 +108,35 @@ function search(opt, getBook) {
     }
   }, function (err, res, body) {
     if (global.debug === true) {
-      console.log(body);
+      console.log(body)
     }
     if (err || (res && res.statusCode !== 200)) {
-      let msg = '';
+      let msg = ''
 
       if (err) {
-        msg = err;
+        msg = err
       }
 
       if (res && res.statusCode) {
-        msg = msg + " " + res.statusCode;
+        msg = msg + " " + res.statusCode
       }
 
       if (getBook) {
-        getBook({msg: msg});
+        getBook({msg: msg})
       }
     } else {
       jsdom.env({
         html: body,
         src: [jquery],
         done: makeJsdomCallback(libraryName, getBook)
-      });
+      })
     }
-  }
-  );
+  })
 }
 
 module.exports = {
   search: search,
   getLibraryNames: function() {
-    return getLibraryNames(libraryList);
+    return getLibraryNames(libraryList)
   }
-};
+}
