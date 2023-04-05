f3a296b0068ce38e15a7eaeed6d6bdf7c703f8c5
afrontend <autoscripts@gmail.com>
2023-04-05 21:26:25 +0900

    Update test for dongne cli

--- test/dongne.spec.js
+++ test/dongne.spec.js
@@ -4,18 +4,24 @@ const assert = require('assert');
 describe('dongnelibrary test', function () {
   describe('search in three libraries', function () {
     this.timeout(20000);
-    const libNameArray = ['여주', '성남', '판교'];
+    const libNameArray = ['여주', '성남', '판교', '영통'];
     it('-l ' + libNameArray.join(','), function (done) {
       dl.search({
         title: 'javascript',
         libraryName: libNameArray
       }, function (err, book) {
+        if  (err) {
+          assert.fail(err.msg);
+        }
         console.log(book.libraryName + ' "' + book.title + '"');
         book.booklist.forEach(function (book) {
           console.log((book.exist?' ✓ ':'  ') +' '+ book.title);
         });
         assert.notEqual(book.booklist.length, 0);
       }, function (err, books) {
+        if  (err) {
+          assert.fail(err.msg);
+        }
         console.log(books.length + ' 개의 도서관을 검색했습니다.');
         console.log('books: ' + JSON.stringify(books, null, 2));
         done();
