2aa818a187c7112b7b06d37c0bc04b63b18e5165
afrontend <autoscripts@gmail.com>
2026-01-24 23:19:15 +0900

    Refactor example.js to use Promise.then style

--- src/example.js
+++ src/example.js
@@ -1,16 +1,14 @@
 const dl = require("dongnelibrary");
-dl.search(
-  {
-    title: "javascript",
-    libraryName: ["여주", "판교"],
-  },
-  function (err, book) {
+
+dl.search({
+  title: "javascript",
+  libraryName: ["여주", "판교"],
+}).then((results) => {
+  results.forEach((book) => {
     console.log(book.libraryName + ' "' + book.title + '"');
-    book.booklist.forEach(function (book) {
-      console.log((book.exist ? " ✓  " : " ✖  ") + " " + book.title);
+    book.booklist.forEach((b) => {
+      console.log((b.exist ? " ✓  " : " ✖  ") + " " + b.title);
     });
-  },
-  function (err, books) {
-    console.log(books.length + " 개의 도서관을 검색했습니다.");
-  },
-);
+  });
+  console.log(results.length + " 개의 도서관을 검색했습니다.");
+});
